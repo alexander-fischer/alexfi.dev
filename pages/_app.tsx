d1c0ae1beefbd668c6fb8abc20c72335b73e3412
alexander-fischer <alexfi@pm.me>
2024-08-27 20:55:40 +0200

    add analytics and speed insights

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,8 @@ import "@/styles/normalize.css"
 import "@/styles/globals.css"
 import type { AppProps } from "next/app"
 import { Poppins } from "next/font/google"
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const font = Poppins({ subsets: ["latin"], weight: "300" })
 
@@ -11,6 +13,8 @@ export default function App({ Component, pageProps }: AppProps) {
             className={`${font.className} bg-slate-50 dark:bg-slate-900 text-gray-800 dark:text-gray-100`}
         >
             <Component {...pageProps} />
+            <Analytics />
+            <SpeedInsights />
         </main>
     )
 }
