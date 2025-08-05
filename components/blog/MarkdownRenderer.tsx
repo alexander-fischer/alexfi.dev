2cfdd4cddc337b67437a3147d21a3be8e247c6b3
Alexander Fischer <alexfi@pm.me>
2025-08-05 08:34:03 +0200

    Add image support for blog posts

--- components/blog/MarkdownRenderer.tsx
+++ components/blog/MarkdownRenderer.tsx
@@ -25,6 +25,12 @@ export default function MarkdownRenderer({ content }: { content: string }) {
                         {...props}
                     />
                 ),
+                img: (props) => (
+                    <img
+                        className="my-4 max-w-full h-auto mx-auto"
+                        {...props}
+                    />
+                ),
                 code({ node, inline, className, children, ...props }: any) {
                     const match = /language-(\w+)/.exec(className || "")
                     return !inline && match ? (
